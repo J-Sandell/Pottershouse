753105dd1c2cea4729579a1a30483de12264b8bc
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-08-31 20:22:48 -0700

    Header and Navbar Progress

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,1 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Navbar, NavItem, Nav, NavLink } from "reactstrap";
+
+
+class Header extends React {
+	render() {
+		return (
+			<Fragment>
+				<div>
+					<Navbar dark sticky="top">
+						<Nav>
+							<NavItem>
+								<NavLink>Home</NavLink>
+							</NavItem>
+						</Nav>
+					</Navbar>
+				</div>
+			</Fragment>
+		)
+
+	}
+}
+
+
+export default Header; 
