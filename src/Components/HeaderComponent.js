763356ebe8450bc9256a4be7bfee96f1637260ec
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-09-01 19:35:10 -0700

    building header and nav

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -6,20 +6,33 @@ class Header extends Component {
 	render() {
 		return (
 			<Fragment>
-				<Nav className="bg-dark">
-					<NavItem>
-						<NavLink>Home</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink>About</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink>Mission</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink>Contact</NavLink>
-					</NavItem>
-				</Nav>
+				<div className="container-fluid">
+					<div className="row">
+						<div className="col">
+							<h1 className="text-center">The Potter's House Christian Fellowship Church</h1>
+						</div>
+						<div className="col-2">
+							<button type="button" class="btn btn-success">Donate</button>
+						</div>
+					</div>
+				</div>
+				<div>
+					<Nav >
+						<NavItem>
+							<NavLink>Home</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink>About</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink>Mission</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink>Contact</NavLink>
+						</NavItem>
+					</Nav>
+				</div>
+
 			</Fragment>
 		)
 
