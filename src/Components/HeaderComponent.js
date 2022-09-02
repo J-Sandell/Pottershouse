21edd6d699f9a8e9d661b95f26440536a34f49a1
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-09-01 20:08:01 -0700

    Header/Navbar work

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -6,32 +6,29 @@ class Header extends Component {
 	render() {
 		return (
 			<Fragment>
+
 				<div className="container-fluid">
 					<div className="row">
 						<div className="col">
 							<h1 className="text-center">The Potter's House Christian Fellowship Church</h1>
 						</div>
-						<div className="col-2">
-							<button type="button" class="btn btn-success">Donate</button>
-						</div>
 					</div>
 				</div>
-				<div>
-					<Nav >
-						<NavItem>
-							<NavLink>Home</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink>About</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink>Mission</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink>Contact</NavLink>
-						</NavItem>
-					</Nav>
-				</div>
+
+				<Nav className="navbar navbar-expand-lg bg-dark">
+					<NavItem>
+						<NavLink>Home</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink>About</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink>Mission</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink>Contact</NavLink>
+					</NavItem>
+				</Nav>
 
 			</Fragment>
 		)
