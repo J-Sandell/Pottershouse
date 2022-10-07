8263f7ca7ed6d90cc89e097f722acc94673a3bb7
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-10-06 19:25:24 -0700

    Working on React router

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,1 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+function Main() {
+	return (
+		<Router>
+			<Head />
+			<Route path="./home" component={Home} />
+			<Route path="./events" component={Event} />
+		</Router>
+	)
+}
