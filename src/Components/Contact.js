6a2e4f41314a8cd9f4dbdf056434a037015b91c9
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-10-09 17:26:00 -0700

    just adding little bit of stuff

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,8 +3,19 @@ import React from "react";
 
 function Contact() {
 	return (
-		<div>
+		<div className="text-center">
 			<h1>Contact</h1>
+
+
+			<p>The Potter's House Christian Fellowship Church<br />
+				727 Fairview Dr St B/C
+				Carson City, NV 89702
+			</p>
+			<p>
+				Service times:
+				Sunday 11 AM and 6 PM
+				Wednesday 7 PM
+			</p>
 		</div>
 	)
 }
