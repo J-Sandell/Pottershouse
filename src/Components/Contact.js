a9a356ad68a4a777167d86c5c15324fd37c447c0
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-10-22 07:23:43 -0700

    light work as of now

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,33 +5,36 @@ import React, { Fragment } from "react";
 function Contact() {
 	return (
 		<Fragment>
-			<div className="card">
-				<img src="/assets/map.png" height={500} width={500} alt="place" />
-				<div className="card-body text-center">
-					<h1>Location:</h1>
-					<h4>
-						727 Fairview Dr St B/C <br />
-						Carson City, NV 89702
-					</h4>
-					<p>
-						Service times:
-						Sunday 11 AM and 6 PM
-						Wednesday 7 PM
-					</p>
+			<div className="d-flex inline">
+				<div className="card">
+					<img src="/assets/map.png" height={500} width={500} alt="place" />
+					<div className="card-body text-center">
+						<h1>Location:</h1>
+						<h4>
+							727 Fairview Dr St B/C <br />
+							Carson City, NV 89702
+						</h4>
+						<p>
+							Service times:
+							Sunday 11 AM and 6 PM
+							Wednesday 7 PM
+						</p>
+					</div>
 				</div>
-			</div>
-			<div className="card">
-				<div className="card-body">
-					<label for="email" className="form-label">Email address</label>
-					<input type="email" className="form-control" id="email" placeholder="emailus@emailus.com"></input>
-				</div>
-				<div className="card-body">
-					<label for="emailwriting" className="form-label">Write us here</label>
-					<textarea className="form-control" id="emailwriting" rows={10}></textarea>
-					<button className="btn btn-primary">Submit</button>
-				</div>
-			
+				<div className="card">
+					<h2 className="text-center">Let us hear from you</h2>
+					<div className="card-body">
+						<label for="email" className="form-label">Email address</label>
+						<input type="email" className="form-control" id="email" placeholder="emailus@emailus.com"></input>
+					</div>
+					<div className="card-body">
+						<label for="emailwriting" className="form-label">Write us here</label>
+						<textarea className="form-control" id="emailwriting" rows={8}></textarea>
+						<button className="btn btn-primary">Submit</button>
+					</div>
 
+
+				</div>
 			</div>
 		</Fragment>
 	)
