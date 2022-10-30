2a45db0db2ae36c72c135eba3860d6eedc9b333a
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-10-30 14:23:53 -0700

    light work

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -9,8 +9,8 @@ function Head() {
 		<Fragment>
 			<div className="container-fluid bg-dark">
 				<div className="row text-center text-white">
-					<div className="col-2">
-						<img className="family" src="/assets/logo2.webp" height={400} width={400} alt="logo " /><br />
+					<div className="family col-2">
+						<img className="" src="/assets/logo2.webp" height={400} width={400} alt="logo " /><br />
 						<p>727 Fairview Dr Suite B/C <br />
 							Carson City, NV 89702 <br />
 						</p>
@@ -19,7 +19,7 @@ function Head() {
 						<h1>The Potter's House Christian Fellowship</h1>
 						<p>"We are a Bible believing Pentecostal Christian church. We believe and preach salvation through repentance of sins and acceptance of Jesus Christ as your Lord and Savior"</p>
 					</div>
-					<div className="col-2">
+					<div className="family col-2">
 						<h3>Service Times:</h3>
 						<h5>Sunday</h5>
 						<p>11 AM and 6 PM</p>
