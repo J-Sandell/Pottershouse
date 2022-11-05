351ac45b12330b16dac09ad330e7e68dcf8b24a1
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-11-04 18:32:29 -0700

    trying new styling + code

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,39 +1,29 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 
 
 function Contact() {
 	return (
-		<Fragment>
-			<div className="d-flex inline">
-				<div className="card">
-					<img className="map" src="/assets/map.png" height={500} width={500} alt="place" />
-					<div className="card-body text-center">
-						<h1>Location:</h1>
-						<h4>
-							727 Fairview Dr St B/C <br />
-							Carson City, NV 89702
-						</h4>
-						<p>
-							Service times:
-							Sunday 11 AM and 6 PM
-							Wednesday 7 PM
-						</p>
-					</div>
-				</div>
-				<div className="card">
-					<h2 className="text-center">Let us hear from you</h2>
-					<div className="card-body">
-						<label for="email" className="form-label">Email address</label>
-						<input type="email" className="form-control" id="email" placeholder="emailus@emailus.com"></input>
-						<label for="emailwriting" className="form-label">Write us here</label>
-						<textarea className="form-control" id="emailwriting" rows={8}></textarea>
-						<button className="btn btn-primary">Submit</button>
-					</div>
 
-				</div>
+		<div className="contact text-center">
+			<div className="">
+				<img className="map" src="/assets/map.png" height={400} width={400} alt="place" />
+				<h1>Location:</h1>
+				<h4>
+					727 Fairview Dr St B/C <br />
+					Carson City, NV 89702
+				</h4>
 			</div>
-		</Fragment>
+			<div className="">
+				<h2 className="text-center">Let us hear from you</h2>
+				<label for="email" className="form-label">Email address</label>
+				<input type="email" className="form-control" id="email" placeholder="emailus@emailus.com"></input>
+				<label for="emailwriting" className="form-label">Write us here</label>
+				<textarea className="form-control" id="emailwriting" rows={8}></textarea>
+				<button className="btn btn-primary">Submit</button>
+			</div>
+		</div>
+
 	)
 }
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,37 +5,37 @@ import { Link } from "react-router-dom";
 function Footer() {
 
 	return (
-		<footer>
-			<div className='container-fluid position-bottom bg-light text-black'>
-				<div className='row offset-1'>
-					<div className='col'>
-						<h3>links</h3>
-						<Link to="/">
-							<i className="bi bi-house-fill">Home</i>
-						</Link>
-						<br />
-						<Link to="/event">
-							<i className="bi bi-calendar-event-fill">Events</i>
-						</Link>
-						<br />
-						<Link to="/contact">
-							<i className="bi bi-person-rolodex">Contact</i>
-						</Link>
-					</div>
-					<div className='col'>
-						<h3>Social</h3>
-						<i className="bi bi-facebook">Facebook</i><br />
-						<i className="bi bi-instagram">Instagram</i>
-					</div>
-					<div className='col'>
-						<h3>Contact</h3>
-						<i className="bi bi-envelope-heart">Email</i><br />
-						<i className="bi bi-geo-fill">Location</i>
 
-					</div>
+		<div className='container-fluid footer--pin bg-light text-black'>
+			<div className='row offset-1'>
+				<div className='col'>
+					<h3>links</h3>
+					<Link to="/">
+						<i className="bi bi-house-fill">Home</i>
+					</Link>
+					<br />
+					<Link to="/event">
+						<i className="bi bi-calendar-event-fill">Events</i>
+					</Link>
+					<br />
+					<Link to="/contact">
+						<i className="bi bi-person-rolodex">Contact</i>
+					</Link>
+				</div>
+				<div className='col'>
+					<h3>Social</h3>
+					<i className="bi bi-facebook">Facebook</i><br />
+					<i className="bi bi-instagram">Instagram</i>
+				</div>
+				<div className='col'>
+					<h3>Contact</h3>
+					<i className="bi bi-envelope-heart">Email</i><br />
+					<i className="bi bi-geo-fill">Location</i>
+
 				</div>
 			</div>
-		</footer>
+		</div>
+
 
 	)
 }
