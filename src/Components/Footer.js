576b54bd7d4a54f12de7600bdf12862e1376e031
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-12-09 19:32:19 -0800

    trying to get footer right

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,24 +6,35 @@ function Footer() {
 
 	return (
 
-		<footer className='site-footer'>
+		<footer>
 			<div className="container">
 				<div className="row">
-					<Link to="/">
-						<i className="bi bi-house-fill">Home</i>
-					</Link>
-					<br />
-					<Link to="/event">
-						<i className="bi bi-calendar-event-fill">Events</i>
-					</Link>
-					<br />
-					<Link to="/contact">
-						<i className="bi bi-person-rolodex">Contact</i>
-					</Link>
-					<i className="bi bi-facebook">Facebook</i><br />
-					<i className="bi bi-instagram">Instagram</i>
-					<i className="bi bi-envelope-heart">Email</i><br />
-					<i className="bi bi-geo-fill">Location</i>
+					<div className="col">
+						<h5>Links</h5>
+						<ul className="list-unstyled">
+							<Link to="/">
+								<i className="bi bi-house-fill">Home</i>
+							</Link>
+							<br />
+							<Link to="/event">
+								<i className="bi bi-calendar-event-fill">Events</i>
+							</Link>
+							<br />
+							<Link to="/contact">
+								<i className="bi bi-person-rolodex">Contact</i>
+							</Link>
+						</ul>
+						<div className="col">
+							<h5>Socials</h5>
+							<i className="bi bi-facebook">Facebook</i><br />
+							<i className="bi bi-instagram">Instagram</i>
+						</div>
+						<div className="col">
+							<h5>Contacts</h5>
+							<i className="bi bi-envelope-heart">Email</i><br />
+							<i className="bi bi-geo-fill">Location</i>
+						</div>
+					</div>
 				</div>
 			</div>
 		</footer>
