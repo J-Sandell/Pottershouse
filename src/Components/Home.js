56409f41e961752749458dfee42add4d9e2fe836
Jeff Sandell <sandelljeffrey88@gmail.com>
2022-12-27 20:41:20 -0800

    fixing into components

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,3 +1,5 @@
+import React from "react";
+import Pastor from "./Pastor";
 
 
 
@@ -8,33 +10,13 @@ function Home() {
 		<div className="container-fluid">
 			<div className="row">
 				<div className="col">
-					<img className="pictures" src="/assets/Family pic.jpg" alt="family" height={350} width={350} ></img>
-					<h2>Pastor's Story</h2>
-					<p>My name is Billy Perez. I am the pastor at the Potter's House church in South Carson City on Fairview Dr. I got saved in 2009  in Reno, NV after years of living in sin. I got involved with gangs at the young age of 12, because I was following the footsteps of my brother and cousins. Even though I was Raised in church and had a mother who seved  God, I rejected that in favor of drugs and gang life. As I got older, gang life got worse and worse. I came close to losing my life, I was in and out of county jail, and I watched my friends get locked up and some even die. At the age of 29 I was at a crossroads. I was in a dark spot and saw that my life had nothing to show for it. Five years previously, my sister had gotten saved. She and her husband had been preaching to me and trying to get me saved as well. I finally realized that I was done with the gang and drug lifestyle. I was ready to give my life to Jesus. The day I came to church and got saved, my pastor presented a challenge. He challenged new believers to wholly serve God for 6 months, an if after 6 months I had nothing to show for it I could walk away. He knew I would never walk away, because it is impossible to not come out changed after being fully devoted to serving God. Now 13 years later I am preaching the gospel. I have a solid family, and my life is blessed.</p>
+					<Pastor/>
 					<h2>Wife's Story</h2>
 					<p>My name is Brooke Perez. My husband is the pastor at the Potter's House Church in South Carson City on Fairview Dr. i got saved when I was 14 years old. MY parents were saved before I was born and my dad became a pastor in Seattle, when i was 2 years old. I was raised in a happy and healthy home, that was blessed by God. Even thought I had seen how living in sin destroyed lives, I had yet to experience it. When I was 12, my family became missionaries to Zambia. This was the beginning of my personal spiritual journey. I got the full realization that I needed my own personal relationship with Jesus when I was 14. At the age of 18, I left Zambia and went to UNR. I was excited to serve God on my own; and make my own way to church rather than go simply because there was no other option. I had high expectations, but the devil had tactics I was not prepared for. I was soon disappointed, and at the age of 21, I told God that i didn't believe that He could make me happy. I almost immediately began to fall into sin. In the next 3 moths I experienced a life without the covering of God and quicly realized that it was not worthit. I rededicated my life to Jesus and I have benn serving Him ever since. Living for God's expectations greatly exceeded any expectations I could have had for myself.
-
 					</p>
 				</div>
 			</div>
 			<div className="row">
-				<div className="col">
-					<h3 className="text">What We Believe:</h3>
-
-					<li>There is only one God, and that He is eternally existent in Three Persons: Father, Son and Holy Spirit.</li>
-					<li>God the Father is the Creator of the universe. He created man in His oun image for fellowship, and called man back to Himself through Christ after the rebellion and fall of man.</li>
-					<li>Jesus Christ is eternally God. He was together with the Father and the Holy Spirit from the beginning, and through Him all things were made. He left Heaven and became incarnated by the Holy Spirit of the Virgin Mary: henceforth, He is forever one Christ with two natures--God and Man--in one Person.</li>
-					<li>The Holy Spirit is God, the Lord, and giver of life, who was active in the Old Testament, and given to the Church in fullness at Pentecost. He empowers the saints for service and witness, cleanses man from the old nature, and conforms us to the image of Christ.</li>
-					<li>The baptism in the Holy Spirit with evidence of speaking in tongues, subsequent to conversion, releases the fullness of the Spirit, and is evidenced by the fruits and gifts of the Holy Spirit.</li>
-					<li>In the Bible containing the Old and New Testament is alone the only infallible, inspired Word of God, and that its authority is ultimate, final, and eternal. It cannot be added to, subtracted from, or superseded in any regard. The Bible is the source of all doctrine, instruction, correction, and reproof. It contains all that is needed for guidance in godliness and practical Christian conduct.</li>
-					<li>Christ's vicarious death on the cross paid the penalty for the sins of the whole world. Its benefits of healing (body, soul, and spirit) are provided for in the atonement as well.</li>
-					<li>That salvation is a free gift of God, based on the merits of the death of His Son, and is appropriated by faith. Salvation is affected by person repentance, belief in the Lord Jesus (justification), and personal acceptance of Him into one's life as Lord and Savior (regeneration).</li>
-					<li>The Christian life is to be one of consecration, devotion, and holiness. The shortcomings of the individual are because of the still progressing sanctification of the saints. The Christian life is filled with trials, tests, and warfare against a spiritual enemy. For those abiding in Christ until their deaths or His return, the promises of eternal blessing in the presence of God are assured.</li>
-					<li>The Church is the Body of Christ, the habitation of God among the saints through the Spirit. Every believer born of the Spirit has a place in the church designated by God. A place where Christ is working in the lives of the called out ones and calling ministers to the great commission to go into all the world and make disciples of every nation.</li>
-					<li>In the ordinances of Baptism and the Lord's Supper. Baptism is the outward sign of what God has already done in the individual's life and is a public testimony that the person now belongs to Christ. It is identifying with the death and resurrection of Jesus and is done in the name of the Father, the Son, and the Holy Spirit. The Lord's Supper is a commemoration of the death of the Lord and is done in remembrance of Him until He comes again; it is a sign of our participation in Him.</li>
-					<li>In the bodily, personal, second coming of the Lord Jesus Christ, the resurrection of the saints, the millennium, and the final judgment. The final judgment will determine the eternal status of both the saints and the unbelievers, determined by their relationship to Jesus Christ.</li>
-
-				</div>
 				<div className="bottompix fluid">
 					<img className="pictures" src="/assets/entrance.jpg" height={200} width={200} alt="Front Door"></img>
 					<img className="pictures" src="/assets/prayerscreen.jpg" height={200} width={200} alt="prayer"></img>
--- src/Components/Pastor.js
+++ src/Components/Pastor.js
@@ -1,6 +1,17 @@
 import React from "react";
 
-class Pastor extends Component {
-
-
+function Pastor() {
+	return (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col">
+					<img className="pictures" src="/assets/Family pic.jpg" alt="family" height={350} width={350} ></img>
+					<h2>Pastor's Story</h2>
+					<p>My name is Billy Perez. I am the pastor at the Potter's House church in South Carson City on Fairview Dr. I got saved in 2009  in Reno, NV after years of living in sin. I got involved with gangs at the young age of 12, because I was following the footsteps of my brother and cousins. Even though I was Raised in church and had a mother who seved  God, I rejected that in favor of drugs and gang life. As I got older, gang life got worse and worse. I came close to losing my life, I was in and out of county jail, and I watched my friends get locked up and some even die. At the age of 29 I was at a crossroads. I was in a dark spot and saw that my life had nothing to show for it. Five years previously, my sister had gotten saved. She and her husband had been preaching to me and trying to get me saved as well. I finally realized that I was done with the gang and drug lifestyle. I was ready to give my life to Jesus. The day I came to church and got saved, my pastor presented a challenge. He challenged new believers to wholly serve God for 6 months, an if after 6 months I had nothing to show for it I could walk away. He knew I would never walk away, because it is impossible to not come out changed after being fully devoted to serving God. Now 13 years later I am preaching the gospel. I have a solid family, and my life is blessed.</p>
+				</div>
+			</div>
+		</div>
+	)
 }
+
+export default Pastor;
