fdf59adde59e9fcdcad93e917b6ce75f09431466
Jeff Sandell <sandelljeffrey88@gmail.com>
2023-01-09 17:47:08 -0800

    stuck on footer on home page

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -12,14 +12,13 @@ function Main() {
 	return (
 		<Fragment>
 			<Header />
-		 	<Routes>
+			<Routes>
 				<Route path="/" exact element={<Home />} />
 				<Route path="/event" element={<Event />} />
 				<Route path="/contact" element={<Contact />} />
 			</Routes>
 			<Footer />
 		</Fragment>
-
 	)
 }
 
